a7d278a695218d524e29426645279ebf9f52b9ab
Brian Adams <bpeteradams@gmail.com>
2021-06-15 17:00:31 -0400

    added new data store to indexeddb

--- public/db.js
+++ public/db.js
@@ -1,4 +1,16 @@
 let db
-let version = 1
+let version = 2
 
 const req = indexedDB.open('BudgetTrackerDB', version)
+req.onerror = (e) => {
+  console.log(`Database Version ${version} has failed to open`);
+}
+req.onsuccess = (e) => {
+  console.log(`Database Version ${version} has been opened`);
+} 
+req.onupgradeneeded = (e) => {
+  db = e.target.result;
+  const {oldVersion, newVersion} = e;
+  console.log(`Database Has Been Upgraded From ${oldVersion} to ${newVersion}`)
+  db.createObjectStore('BudgetTrackerStore')
+}
