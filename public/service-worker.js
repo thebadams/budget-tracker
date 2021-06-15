01ec00e0453496ac7d0f15d95d69eb04fedc0be3
Brian Adams <bpeteradams@gmail.com>
2021-06-15 16:02:06 -0400

    got files successfully cached

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,41 @@
-console.log("Service Worker Has Been Linked!")
+let version = 4
+
+const CACHE_NAME = `cache-v${version}`
+const FILES_TO_CACHE =[
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/manifest.webmanifest",
+  "/index.js",
+  "/service-worker.js",
+  "icons/icon-192x192.png",
+  "icons/icon-512x512.png"
+]
+
+self.addEventListener('install', function(e) {
+  console.log('Service Worker Installed')
+   e.waitUntil(
+      caches.open(CACHE_NAME)
+        .then((cache) => {
+          console.log(`${CACHE_NAME} has been opened`)
+          cache.addAll(FILES_TO_CACHE)
+        }))
+  self.skipWaiting();
+
+})
+
+self.addEventListener('activate', function(e) {
+  console.log('Service Worker Activated')
+})
+ 
+// caches.open(CACHE_NAME)
+//   .then(cache=> {
+//     console.log(`${CACHE_NAME} Has Been Opened`)
+//     cache.add(FILES_TO_CACHE)
+//     cache.keys()
+//       .then(keys=>{
+//         keys.forEach((key)=> {
+//           console.log(key)
+//         })
+//       })
+//  })
